1a9f974709f7bf54df26e908acf1de459d99f370
sagar-shetty21 <5agarm5hetty05@gmail.com>
2025-09-10 07:12:52 +0530

    dashboard error fixed

--- utils/contexts/GlobalSearchContext.tsx
+++ utils/contexts/GlobalSearchContext.tsx
@@ -1,38 +1,48 @@
 import React, {
-  createContext,
-  useContext,
-  useState,
-  type ReactNode,
+    createContext,
+    useContext,
+    useState,
+    type ReactNode,
 } from "react";
 
 type GlobalSearchContextType = {
-  search: string;
-  isActive: boolean;
-  setIsActive: (value: boolean) => void;
-  setSearch: (value: string) => void;
+    search: string;
+    isActive: boolean;
+    setIsActive: (value: boolean) => void;
+    setSearch: (value: string) => void;
 };
 
 const GlobalSearchContext = createContext<GlobalSearchContextType | undefined>(
-  undefined
+    undefined
 );
 
 export function GlobalSearchProvider({ children }: { children: ReactNode }) {
-  const [search, setSearch] = useState("");
-  const [isActive, setIsActive] = useState(false);
+    const [search, setSearch] = useState("");
+    const [isActive, setIsActive] = useState(false);
 
-  return (
-    <GlobalSearchContext.Provider
-      value={{ search, isActive, setIsActive, setSearch }}
-    >
-      {children}
-    </GlobalSearchContext.Provider>
-  );
+    const value = React.useMemo(
+        () => ({
+            search,
+            isActive,
+            setIsActive,
+            setSearch,
+        }),
+        [search, isActive]
+    );
+
+    return (
+        <GlobalSearchContext.Provider value={value}>
+            {children}
+        </GlobalSearchContext.Provider>
+    );
 }
 
 export function useGlobalSearch() {
-  const context = useContext(GlobalSearchContext);
-  if (!context) {
-    throw new Error("useSearch must be used within a GlobalSearchProvider");
-  }
-  return context;
+    const context = useContext(GlobalSearchContext);
+    if (!context) {
+        throw new Error(
+            "useGlobalSearch must be used within a GlobalSearchProvider"
+        );
+    }
+    return context;
 }
